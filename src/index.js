1895be6458b396337a1194a709d68ea8a42c6668
nhinojosa <nhinojosa@southtexascollege.edu>
2023-09-23 11:26:19 -0500

    Added Express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello")
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (req,res) => {
+    res.send({msg: "Hello There"})
+})
+
+
+app.listen(port)
