e38c9396821a01772d6663acb166b855a2363f8b
nhinojosa <nhinojosa@southtexascollege.edu>
2023-09-23 23:02:10 -0500

    removed export keyword

--- src/helpers/errors.js
+++ src/helpers/errors.js
@@ -1,14 +1,14 @@
 //import logger from './logger'
 const logger = require('./logger')
 
-export const notFound = (req, res, next) => {
+const notFound = (req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`)
   res.status(404)
   next(error)
 }
 
 // eslint-disable-next-line no-unused-vars
-export const errorHandler = (error, req, res, next) => {
+const errorHandler = (error, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode
 
   logger.error(new Error(error.message))
